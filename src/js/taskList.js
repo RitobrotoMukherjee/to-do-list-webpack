f99af4292a86c15f9f7f517d671664ade6044781
RitobrotoMukherjee <ritobrotomukherjee1991@gmail.com>
2022-06-03 11:19:55 +0530

    Implement reorder task logic using both map and forEach for learning purposes

--- src/js/taskList.js
+++ src/js/taskList.js
@@ -1,6 +1,8 @@
 /* eslint-disable */
 /* Main Class to set local storage */
 
+import Task from "./task";
+
 export default class {
   setList(tasks) {
     return localStorage.setItem('tasks', JSON.stringify(tasks));
@@ -11,7 +13,12 @@ export default class {
   }
 
   reOrderTaskIndex(tasks) {
-    tasks.forEach((task, i) => task.index = i + 1);
+    // using destructuring and map
+    // const reorderedTasks = tasks.map(({ completed, description }, i) => new Task(i + 1, completed, description) );
+    // return this.setList(reorderedTasks);
+
+    //using forEach
+    tasks.forEach((task, i) => task.index = i + 1 );
     return this.setList(tasks);
   }
 }
